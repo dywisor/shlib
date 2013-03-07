f8ef3f66f171e9f8a38a5d1bbcbd9dfbd60e2dcc
André Erdmann <dywi@mailerd.de>
2013-03-07 21:53:45 +0100

    lib/test: modules for testing
    This commit adds some assert_ functions, namely assert_retcode()
    and assert_pipe() (the latter one only for bash).
    test/assert is a virtual module that depends on all other assert
    modules that are available for the chosen shell interpreter.

---
+++ lib/test/assert_pipe.bash
@@ -0,0 +1,8 @@
+# void assert_pipe ( expected_value=0 )
+#
+#  Dies if PIPESTATUS != expected_value.
+#
+assert_pipe() {
+   local -i p="${PIPESTATUS[0]}" c="${1:-0}"
+   [[ ${p} -eq ${c} ]] || function_die "expected ${c}, but got ${p}."
+}
