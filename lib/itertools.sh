0a2fa4c52614f7e096e15f6b02adff6f3b810e09
André Erdmann <dywi@mailerd.de>
2013-03-07 21:30:50 +0100

    lib/itertools: fixes and more iterator functions

--- lib/itertools.sh
+++ lib/itertools.sh
@@ -1,3 +1,15 @@
+# itertools
+#  create/parse lists and entities that can be perceived as such,
+#  e.g. directory/filesystem trees, file text lines, ...
+#
+
+# @private int __itertools_kw_is_not ( word, **kw )
+#
+#  Returns true if kw != word else false.
+#
+__itertools_kw_is_not() {
+   [ "x${kw?}" != "x$*" ]
+}
 
 # void generic_iterator (
 #    item_separator, *words,
@@ -39,17 +51,24 @@ generic_iterator() {
 }
 # --- end of generic_iterator (...) ---
 
-# void itertools_print_item ( *items )
+# void itertools_print_item ( item )
 #
-#  Prints a string representation of zero or more items.
-#  Meant for testing.
+#  Prints a string representation of an item. Meant for testing.
 #
 itertools_print_item() {
-   if [ "x${*}" = "x${1:-}" ]; then
+   if [ -z "${2-}" ]; then
       echo "item<${*}>"
    else
       echo "items<${*}>"
    fi
+}
+
+# void itertools_print_fs_item ( item )
+#
+#  Prints a string representation of a filesystem item (file/dir).
+#
+itertools_print_fs_item() {
+   echo "fs_item<path='${1-}' parent_dir='${2-}' name='${3-}' type='${4-}'>"
 }
 
 # void eval_iterator ( func_name, item_separator )
@@ -79,6 +98,144 @@ dot_iterator() {
 # @iterator <default> default_iterator
 default_iterator() {
    generic_iterator "${IFS_DEFAULT?}" "$@"
+}
+
+
+# @iterator <file> file_iterator ( **ITER_SKIP_COMMENT=y )
+#
+#  Reads zero or more files and calls F_ITER for each line.
+#
+file_iterator() {
+   local line
+   while [ $# -gt 0 ]; do
+      while read line; do
+         if [ -z "${line}" ] && [ "${ITER_SKIP_EMPTY:-y}" = "y" ]; then
+            true
+         elif \
+            [ "${ITER_SKIP_COMMENT:-y}" ] && [ "x${line#\#}" != "x${line}" ]
+         then
+            true
+         elif [ "${ITER_UNPACK_ITEM:-n}" = "y" ]; then
+            ${F_ITER:-echo} ${line}   || ${F_ITER_ON_ERROR:-return}
+         else
+            ${F_ITER:-echo} "${line}" || ${F_ITER_ON_ERROR:-return}
+         fi
+      done < "${1:?}"
+      shift
+   done
+}
+
+
+# @iterator <filesystem> fs_iterator (
+#    *dirs,
+#    ...,
+#    **F_ITER_DIR_MISSING=function_die
+# )
+#
+# For each <dir> in dirs:
+#  Iterates over the entries of a filesystem tree starting at <dir>.
+#
+#
+fs_iterator() {
+   local d
+   for d; do
+      if [ -d "${d%/}/" ]; then
+         line_iterator "$( find ${d%/}/ )"
+      elif [ -n "${F_ITER_DIR_MISSING-}" ]; then
+         ${F_ITER_DIR_MISSING} "${d%/}"
+      else
+         function_die "no such directory: ${d}"
+      fi
+   done
+}
+
+
+# void dir_iterator (
+#    *dirs,
+#    **F_ITER=echo,
+#    **F_ITER_FILE=(F_ITER),
+#    **F_ITER_DIR=(F_ITER),
+#    **F_ITER_ON_ERROR=return,
+#    **F_ITER_DIR_MISSING=function_die,
+#    **ITER_IGNORE_SYMLINK=n,
+#    **ITER_ABSPATH=y,
+#    **ITER_ENTRY_PREFIX="",
+#    **ITER_ENTRY_SUFFIX="",
+# )
+# DEFINES @dir_iterator <fixed keywords> <function name> (
+#    *dirs,
+#    <variable keywords>
+# )
+#
+# Iterates over the content of zero or more directories and calls
+# F_ITER_FILE / F_ITER_DIR / F_ITER for each file / dir / ...
+#
+# Each item function (F_ITER*) has to accept 4 args,
+# 1: path to the item
+# 2: path to the directory containing the item
+# 3: the item's name
+# 4: the item's type "file" for file, "dir" for directory, "" for unknown
+#
+# Not recursive.
+#
+dir_iterator() {
+   local d f
+   while [ $# -gt 0 ]; do
+      if [ -n "${1}" ]; then
+         if [ "${ITER_ABSPATH:-y}" = "y" ]; then
+            d=`realpath "${1}"`
+         else
+            d="${1}"
+         fi
+
+         if [ -d "${d}" ]; then
+
+            for f in "${d}"/${ITER_ENTRY_PREFIX-}*${ITER_ENTRY_SUFFIX-}; do
+               if [ "$f" != "${d}/${ITER_ENTRY_PREFIX-}*${ITER_ENTRY_SUFFIX-}" ]; then
+                  # else empty dir
+                  if \
+                     [ -h "${f}" ] && [ "${ITER_IGNORE_SYMLINK:-n}" = "y" ]
+                  then
+                     true
+                  elif [ -f "${f}" ]; then
+                     ${F_ITER_FILE:-${F_ITER:-echo}} \
+                        "${f}" "${d}" "${f##*/}" "file"
+
+                  elif [ -d "${f}" ]; then
+                     ${F_ITER_DIR:-${F_ITER:-echo}} \
+                        "${f}" "${d}" "${f##*/}" "dir"
+                  elif [ -n "${F_ITER-}" ]; then
+                     # char/block dev, broken symlinks, ...
+                     ${F_ITER} "${f}" "${d}" "${f##*/}" ""
+                  fi
+               fi
+            done
+
+         elif [ -n "${F_ITER_DIR_MISSING:-}" ]; then
+            ${F_ITER_DIR_MISSING} "${1}" "${d}"
+         else
+            function_die "no such directory: ${1}"
+         fi
+      fi
+      shift
+   done
+}
+
+# @dir_iterator F_ITER_DIR=<self> recursive_dir_iterator ( dir )
+#
+recursive_dir_iterator() {
+   F_ITER_DIR=recursive_dir_iterator dir_iterator "${1:?}"
+}
+
+# int linelist_has ( word, list )
+#
+#  Returns true if word is in the given list (list items separated by
+#  a newline char) else false.
+#
+linelist_has() {
+   local kw="${1:-}"
+   shift && F_ITER=__itertools_kw_is_not line_iterator "$@" || return 0
+   return 1
 }
 
 
@@ -117,5 +274,5 @@ generic_list_join() {
 #  Creates @list_join functions.
 #
 eval_list_join() {
-   eval "${1:?}() { generic_iterator \"${2?}\" \"\$@\"; }"
+   eval "${1:?}() { generic_list_join \"${2?}\" \"\$@\"; }"
 }
---
+++ lib/list_has.sh
@@ -0,0 +1,12 @@
+# int list_has ( word, *list_items )
+#
+#  Returns true if word is in list_items, else false.
+#
+list_has() {
+   local kw="${1:?}"
+
+   while [ $# -gt 0 ] && shift; do
+      [ "x${kw}" != "x${1:-}" ] || return 0
+   done
+   return 1
+}
