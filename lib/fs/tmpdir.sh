df07f64d2df20e3d2202f2a2d0471fa35122e921
André Erdmann <dywi@mailerd.de>
2013-03-07 21:46:27 +0100

    lib/fs/tmpdir: get temporary directories
    get_tmpdir() creates a temporary directory that will be automatically
    wiped at exit.

---
+++ lib/fs/tmpdir.sh
@@ -0,0 +1,27 @@
+
+# int tmpdir_cleanup ( dir=[T=""] )
+#
+# Removes the given directory (if it exists). (For atexit).
+#
+tmpdir_cleanup() {
+   local d="${1:-${T:-}}"
+   if [ -n "${d}" ] && [ -d "${d}" ]; then
+      rm -r "${d}"
+   fi
+}
+
+# int get_tmpdir ( tmpdir_suffix, **GET_TMPDIR_QUIET=y )
+#
+# Creates a temporary directory that will be wiped at exit.
+# The dir will be stored in T and printed to stdout unless GET_TMPDIR_QUIET
+# is set to 'y'.
+#
+get_tmpdir() {
+   T=`mktemp -d "${TMPDIR:-/tmp}/${1:-shlib}.XXXXXXXXXX"`
+   if [ -n "${T}" ]; then
+      atexit_register tmpdir_cleanup "${T}"
+      [ "${GET_TMPDIR_QUIET:-y}" = "y" ] || echo "${T}"
+   else
+      return 1
+   fi
+}
