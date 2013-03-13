26d46ba5f297d55b5055c64759bd4c8f417195b6
André Erdmann <dywi@mailerd.de>
2013-03-13 22:35:31 +0100

    lib/fs/remount: remount_ro()/remount_rw()

---
+++ lib/fs/remount.sh
@@ -0,0 +1,26 @@
+# int remount ( *mp, **REMOUNT_MODE ), raises function_die()
+#
+#  Remounts the given mountpoints. Returns on first failure.
+#
+remount() {
+   [ -n "${REMOUNT_MODE-}" ] || function_die "REMOUNT_MODE must be set."
+   while [ $# -gt 0 ]; do
+      if [ -n "${1-}" ]; then
+         veinfo "Remounting ${1} with mode=${REMOUNT_MODE}"
+         mount "${1}" -o remount,${REMOUNT_MODE} || return
+      fi
+      shift
+   done
+}
+
+# int remount_ro ( *mp )
+#
+#  Remounts the given mountpoints readonly.
+#
+remount_ro() { REMOUNT_MODE="ro" remount "$@"; }
+
+# int remount_rw ( *mp )
+#
+#  Remounts the given mountpoints in read-write mode.
+#
+remount_rw() { REMOUNT_MODE="rw" remount "$@"; }
