83777027d732a449770251ab4d046e2d7c2230dc
André Erdmann <dywi@mailerd.de>
2013-03-13 22:39:54 +0100

    lib/strutil/: charwise()

---
+++ lib/strutil/charwise.bash
@@ -0,0 +1,20 @@
+# int charwise ( func, chars, [*argv] )
+#
+#  Runs func <char> *argv for each char in chars.
+#
+#  Returns on first failure.
+#
+charwise() {
+   [[ $# -ge 2 ]] || return 0
+   local func="${1:?}" word="${2-}"
+   shift 2
+
+   local -i i=0
+   local c="${word:${i}:1}"
+   while [[ "${c}" ]]; do
+      [[ -z "${c# }" ]] || ${func} "${c}" "$@" || return
+      ((i++)) || :
+      c="${word:${i}:1}"
+   done
+   return 0
+}
---
+++ lib/strutil/charwise.sh
@@ -0,0 +1,27 @@
+# int charwise ( func, chars, [*argv] )
+#
+#  Runs func <char> *argv for each char in chars.
+#
+#  Returns on first failure.
+#
+charwise() {
+   [ $# -ge 2 ] || return 0
+   local func="${1:?}" chars="${2-}" c
+   shift 2
+
+   # using fold is safer and faster, but it could be unavailable in
+   # initramfs systems (busybox et al.)
+   if qwhich fold; then
+      for c in `echo "${chars}" | fold -w1`; do
+         [ -z "${c# }" ] || ${func} "${c}" "$@" || return
+      done
+   else
+      c=`echo "${chars}" | cut -c 1`
+      while [ -n "${chars}" ]; do
+         [ -z "${c# }" ] || ${func} "${c}" "$@" || return
+         chars="${chars#${c}}"
+         c=`echo "${chars}" | cut -c 1`
+      done
+   fi
+   return 0
+}
