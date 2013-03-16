72cc0a6e7458270ca2e5a28ef70461f0ee750002
André Erdmann <dywi@mailerd.de>
2013-03-16 22:44:10 +0100

    lib/fs: mountutil
    this module adds misc mount-related functions
    
    currently, it provides the unmount_if_mounted( mp, [dev] ) function
    which unmounts a mounted only if a certain device (or any) is mounted
    at the given mountpoint/directory.

---
+++ lib/fs/mountutil.sh
@@ -0,0 +1,21 @@
+# int unmount_if_mounted ( mp, [dev] )
+#
+#  Unmounts mp if
+#  * dev is mounted at mp
+#  * dev is not set and a device is mounted at mp
+#
+#  Returns 0 ifnothing done (mp not mounted or mp not specified),
+#  else umount's return value, which is 0 if mp has been unmounted.
+#
+unmount_if_mounted() {
+   disk_mounted "${2-}" "${1-}" || return 0
+   umount "${1}"
+}
+
+# @function_alias umount_if_mounted() renames unmount_if_mounted (...)
+#
+umount_if_mounted() { unmount_if_mounted "$@"; }
+
+# @function_alias unmount() renames umount (...)
+#
+unmount() { umount "$@"; }
