df7548b55c85ffce288f720a11077e05a2a96bcf
André Erdmann <dywi@mailerd.de>
2013-03-25 01:04:39 +0100

    lib/die: wrap autodie(), print function backtrace

--- lib/die.sh
+++ lib/die.sh
@@ -1,10 +1,12 @@
 if [ -z "${__HAVE_SHLIB_DIE__:-}" ]; then
 readonly __HAVE_SHLIB_DIE__=y
 
-# @noreturn die ( message=, code=2, **DIE=exit, **F_ON_DIE= )
+# @noreturn die ( message=, code=2, **DIE=exit, **F_ON_DIE=, **PRINT_FUNCTRACE=y )
 #
 #  if %F_ON_DIE has is not defined / has a null value:
 #   Prints %message to stderr and calls %DIE(code) afterwards.
+#   Also prints the function trace if it is available (bash) and
+#   PRINT_FUNCTRACE is set to 'y'
 #  else:
 #   Calls %F_ON_DIE ( message, code ). Does the actions above
 #   only if %F_ON_DIE() returns a non-zero value.
@@ -17,6 +19,9 @@ die() {
          eerror "${1}" "died:"
       else
          eerror "" "died."
+      fi
+      if [ "${PRINT_FUNCTRACE:-y}" = "y" ] && [ -n "${FUNCNAME-}" ]; then
+         print_functrace eerror
       fi
       ${DIE:-exit} ${2:-2}
    fi
@@ -36,16 +41,27 @@ die__function() {
    fi
 }
 
-# void autodie ( *argv )
+# @private void die__autodie ( *argv )
 #
 #  Runs *argv. Dies on non-zero return code.
 #
-autodie() {
+die__autodie() {
    if "$@"; then
       return 0
    else
       die "command '$*' returned $?."
    fi
 }
+
+# void autodie ( *argv, **AUTODIE=die__autodie )
+#
+#  Runs AUTODIE ( *argv ) which is supposed to let the script die on
+#  non-zero return code.
+#
+autodie() { ${AUTODIE:-die__autodie} "$@"; }
+
+# @function_alias run() copies autodie()
+#
+run() { ${AUTODIE:-die__autodie} "$@"; }
 
 fi
