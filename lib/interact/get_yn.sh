cabab70d1368a8fe4ed7ed68bae038c3ef43377a
André Erdmann <dywi@mailerd.de>
2013-03-28 00:57:36 +0100

    lib/interact: get_yn()

---
+++ lib/interact/get_yn.sh
@@ -0,0 +1,18 @@
+# int get_yn ( prompt, char_y, char_n )
+#
+#  Asks the user a yes/no question and expects a single char as response.
+#  Returns true if the response was char_y and false if char_n.
+#
+#  Repeats asking until a valid answer has been typed in.
+#
+get_yn() {
+   set -- "${1-}${1:+ }" "${2:-y}" "${3:-n}"
+
+   local prompt="${1}(${2}/${3}) " yn=
+   while [ "${yn}" != "${2}" ] && [ "${yn}" != "${3}" ]; do
+      echo -n "${prompt}"
+      read -n1 yn
+      echo
+   done
+   [ "${yn}" = "${2}" ]
+}
