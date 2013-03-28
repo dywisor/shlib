f4ef24ec793c9bd60a0f225fa105a3f92a2b18e3
André Erdmann <dywi@mailerd.de>
2013-03-28 01:16:30 +0100

    lib/strutil: stdstr

---
+++ lib/strutil/stdstr.sh
@@ -0,0 +1,34 @@
+# @function_alias void str_trim() renames sed()
+#
+#  Removes whitespace at the beginning + end of a string
+#  and replaces any whitespace sequence within the string
+#  with a single space char.
+#
+str_trim() { sed -r -e 's,^\s+,,' -e 's,\s+$,,' -e 's,\s+, ,g'; }
+
+# @function_alias void str_strip() renames sed()
+#
+#  Removes whitespace at the beginning + end of a string.
+#
+str_strip() { sed -r -e 's,^\s+,,' -e 's,\s+$,,'; }
+
+# @function_alias void str_rstrip() renames sed()
+#
+#  Removes whitespace at the end of a string.
+#
+str_rstrip() { sed -r -e 's,\s+$,,'; }
+
+# @function_alias void str_lstrip() renames sed()
+#
+#  Removes whitespace at the beginning of a string.
+#
+str_lstrip() { sed -r -e 's,^\s+,,'; }
+
+# @function_alias str_field ( fieldspec, field_separator=" " ) renames cut()
+str_field() { cut -d "${2- }" -f "${1}"; }
+
+# @function_alias str_upper() renames tr()
+str_upper() { tr [:lower:] [:upper:]; }
+
+# @function_alias str_lower() renames tr()
+str_upper() { tr [:upper:] [:lower:]; }
