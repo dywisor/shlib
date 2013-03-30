b7f5d82c16e068a0fda9f91bce9783c4cc0d1a52
André Erdmann <dywi@mailerd.de>
2013-03-30 17:52:27 +0100

    scripts/.../telinit: optionally act as wrapper

--- scripts/initramfs/telinit.sh
+++ scripts/initramfs/telinit.sh
@@ -6,14 +6,20 @@ for p in klogd syslogd; do
 done
 
 # unmount what's left
-/bin/busybox umount -a -r
-/bin/busybox swapoff -a
+/bin/busybox sync
 
-case "${1-}" in
-   0)
-      /bin/busybox poweroff -f
-   ;;
-   6)
-      /bin/busybox reboot -f
-   ;;
-esac
+if [ ! -e /proc/self ] || [ -e /proc/swaps ]; then
+   /bin/busybox swapoff -a
+fi
+
+/bin/busybox umount -n -a -r
+
+# call real telinit (if any) or poweroff/reboot
+
+if [ "${0}" != "/sbin/telinit" ] && [ -x "/sbin/telinit" ]; then
+   /sbin/telinit "$@"
+elif [ "${1-}" = "0" ]; then
+   /bin/busybox poweroff -f
+elif [ "${1-}" = "6" ]; then
+   /bin/busybox reboot -f
+fi
