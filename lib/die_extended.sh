764b8ec17dd107444021eb17aadd1534d24aecab
André Erdmann <dywi@mailerd.de>
2013-03-30 17:47:34 +0100

    split autodie() from die_extended

--- lib/die_extended.sh
+++ lib/die_extended.sh
@@ -31,27 +31,4 @@ die__extended() {
 # make die__extended() available
 __F_DIE=die__extended
 
-# @private void die__autodie ( *argv )
-#
-#  Runs *argv. Dies on non-zero return code.
-#
-die__autodie() {
-   if "$@"; then
-      return 0
-   else
-      die "command '$*' returned $?."
-   fi
-}
-
-# void autodie ( *argv, **AUTODIE=die__autodie )
-#
-#  Runs AUTODIE ( *argv ) which is supposed to let the script die on
-#  non-zero return code.
-#
-autodie() { ${AUTODIE:-die__autodie} "$@"; }
-
-# @function_alias run() copies autodie()
-#
-run() { ${AUTODIE:-die__autodie} "$@"; }
-
 fi
