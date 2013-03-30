4e625edcb730b54666befbd4bcc18617e71bf29c
André Erdmann <dywi@mailerd.de>
2013-03-30 23:09:16 +0100

    lib/function/self: safely call the main function

---
+++ lib/function/self.sh
@@ -0,0 +1,16 @@
+# int as_function ( *argv, **MAIN_FUNCTION=**SCRIPT_NAME )
+#
+#  Calls a function named MAIN_FUNCTION or SCRIPT_NAME.
+#  Scripts can use this in order to avoid infinite recursion.
+#
+as_function() {
+   local func="${MAIN_FUNCTION:-${SCRIPT_NAME:?}}"
+   if function_defined "${func}"; then
+      ${func} "$@"
+   else
+      die "main function ${func} is not defined."
+   fi
+}
+
+# @function_alias asf() renames as_function()
+asf() { as_function "$@"; }
