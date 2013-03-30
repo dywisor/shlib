a683d2dd932c1aa72f8e1e0a5a2f4a4d1042d703
André Erdmann <dywi@mailerd.de>
2013-03-30 23:08:24 +0100

    lib/scriptinfo: make variables readonly

--- lib/scriptinfo.sh
+++ lib/scriptinfo.sh
@@ -1,3 +1,3 @@
-SCRIPT_FILENAME="${0##*/}"
-SCRIPT_NAME="${SCRIPT_FILENAME%.*}"
-SCRIPT_DIR="${0%/*}"
+readonly SCRIPT_FILENAME="${0##*/}"
+readonly SCRIPT_NAME="${SCRIPT_FILENAME%.*}"
+readonly SCRIPT_DIR="${0%/*}"
