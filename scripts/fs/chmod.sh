40aedfedf450099e00c18fd3b8c609072b9ef1cb
André Erdmann <dywi@mailerd.de>
2013-04-01 19:09:58 +0200

    scripts/fs/chmod: add multicall error message

--- scripts/fs/chmod.sh
+++ scripts/fs/chmod.sh
@@ -1,2 +1,11 @@
-chmod_normalize_mode "${SCRIPT_NAME}" && \
-chmod --preserve-root ${v0} "$@"
+MULTICALL_ERROR_MSG="You have set up symlinks to this script rather than executing it directly."
+
+if [ "${SCRIPT_NAME}" != "chmod" ]; then
+   chmod_normalize_mode "${SCRIPT_NAME}" && \
+   chmod --preserve-root ${v0} "$@"
+elif [ "${HAVE_SHLIB_ALL:-n}" = "y" ]; then
+   die "${MULTICALL_ERROR_MSG}" 5
+else
+   echo "${MULTICALL_ERROR_MSG}" 1>&2
+   exit 5
+fi
