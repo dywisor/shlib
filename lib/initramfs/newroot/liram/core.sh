6930ae6a437ad9257bc80c8965aebab82c2433fe
André Erdmann <dywi@mailerd.de>
2013-04-11 22:36:19 +0200

    newroot/liram: core
    The core module is on what liram layouts should depend.

---
+++ lib/initramfs/newroot/liram/core.sh
@@ -0,0 +1,123 @@
+## This module must not depend on any file from ./layout/
+
+# You probably want to include ./layouts and not this module (./core).
+
+## functions from fs/filesize
+
+# @extern int get_filesize ( fs_item )
+#
+#  Determines the size of fs_item (in MiB) and stores the result in %FILESIZE.
+#
+
+
+## functions from initramfs/newroot/base
+
+# @extern void newroot_doprefix ( fspath, **NEWROOT )
+# @extern void newroot_detect_home_dir ( force=n, **NEWROOT_HOME_DIR! )
+
+
+## functions from initramfs/newroot/liram/base
+
+# @extern void liram_init(), raises *die()
+#
+#  This function does everything that is needed to initialize and populate
+#  a rootfs in ram (NEWROOT as tmpfs).
+#
+#  External modules should use this function after setting variables
+#  like LIRAM_DISK, LIRAM_LAYOUT etc..
+#
+#  In fact, this is the only function that non-liram modules should care
+#  about.
+#
+
+# @extern ~int liram_die          (...) wraps initramfs_die (...)
+# @extern ~int liram_populate_die (...) wraps liram_die (...)
+
+# @extern int liram_populate ( **LIRAM_LAYOUT=default ), raises liram_die()
+#
+#  Populate NEWROOT using the configured layout (or the default one).
+#
+#  Also sets variables required for population,
+#  e.g. TARBALL_SCAN_DIR and SLOT.
+#
+
+
+## functions from initramfs/newroot/liram/util
+
+# @extern void liram_scan_files()
+#
+#  Searches for files that can be used to populate NEWROOT.
+#
+
+# @extern int liram_get_tarball  ( name )
+# @extern int liram_get_squashfs ( name )
+# @extern int liram_get_sfs      ( name )
+#
+#  Get files by name. The (absolute) filepath will be stored in %v0.
+#
+
+# @extern int liram_unpack_name ( name, dest="" )
+#
+#  Unpacks a tarball referenced by name into NEWROOT/dest.
+#
+
+# @extern int liram_unpack_replace_name (
+#    name, dest="", backup="", delete_backup=""
+# )
+#
+#  Like liram_unpack_name(), but replaces NEWROOT/dest instead of
+#  overwriting it.
+#
+
+# @extern int liram_unpack_default ( name, tarball_file=<detect> ),
+#  raises initramfs_die()
+#
+#  Unpacks a tarball into its default directory (depending on name).
+#  Optionally searches for the tarball if no second arg supplied.
+#
+#  Returns 0 on success.
+#
+
+# @extern int liram_unpack_name_default ( *name ), raises initramfs_die()
+#
+#  Unpacks zero or more tarballs referenced by name into their default
+#  destition (e.g. "rootfs" => "/", "usr" => "/usr").
+#
+#  Returns on first failure.
+#
+
+# @extern int liram_unpack_all_default(), raises initramfs_die()
+#
+#  Unpacks whatever available to its default location.
+#
+#  !!! This will fail if you add unknown entries to the TARBALL_SCAN_NAMES
+#      variable.
+#
+
+# @extern int liram_unpack_optional (
+#    name, tarball_file=<detect>, dest=<detect>, **v0!
+# )
+#
+#  Unpacks an optional tarball using whatever function that fits,
+#  either newroot_unpack_tarball() or liram_unpack_default().
+#
+#  Returns 0 if a tarball has been extracted, else 1 (no such tarball).
+#
+#  Note: While existence of the file is optional, unpacking is not.
+#        This function will die if the tarball exists but cannot be extracted.
+#
+
+# @extern int liram_sfs_container_import()
+
+# @extern void newroot_sfs_container_init()
+# @extern int  newroot_sfs_container_mount()
+# @extern int  sfs_container_avail()
+# @extern int  sfs_container_downsize()
+# @extern int  sfs_container_finalize()
+# @extern int  sfs_container_import()
+# @extern void sfs_container_init()
+# @extern int  sfs_container_lock()
+# @extern int  sfs_container_mount()
+# @extern int  sfs_container_unlock()
+# @extern int sfs_container_mount_writable()
+# @extern int newroot_sfs_container_mount_writable()
