c418bf39c552651c9e1bdd62f764908728c23f78
André Erdmann <dywi@mailerd.de>
2013-04-11 21:34:48 +0200

    misc/busybox: is_busybox_command()

--- lib/misc/busybox.sh
+++ lib/misc/busybox.sh
@@ -12,3 +12,30 @@ busybox_overlay() {
       return 0
    fi
 }
+
+# int is_busybox_command ( command )
+#
+#  Returns 0 if command (either a name, e.g. "ls" or a path, e.g. "/bin/ls")
+#  is a symlink to busybox, else a non-zero value will be returned.
+#
+is_busybox_command() {
+   case "${1-}" in
+      '')
+         return 2
+      ;;
+      /*)
+         if [ -h "${1}" ]; then
+            local link_target=`readlink -f "${1}"`
+            [ "${link_target}" = "${BUSYBOX}" ] || return 1
+
+         elif [ "${1}" != "${BUSYBOX}" ]; then
+            return 1
+         fi
+      ;;
+      *)
+         # resolve by recursion
+         is_busybox_command "$(which ${1})" || return
+      ;;
+   esac
+   return 0
+}
