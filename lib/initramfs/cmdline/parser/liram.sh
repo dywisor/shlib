e101d89aec999e086c07e58ae4e2b475cd72b85a
André Erdmann <dywi@mailerd.de>
2013-04-18 00:21:23 +0200

    initramfs/cmdline/parser: liram

---
+++ lib/initramfs/cmdline/parser/liram.sh
@@ -0,0 +1,73 @@
+__CMDLINE_ARGPARSE_FUNCTIONS="${__CMDLINE_ARGPARSE_FUNCTIONS-}
+cmdline_parser_liram
+"
+
+__cmdline_parser_liram_opts() {
+   local k="${1%%=*}" v="${v#*=}"
+   [ "${v}" != "${k}" ] || v=
+
+   case "${k}" in
+      'disk'|'sysdisk')
+         LIRAM_DISK="${v}"
+      ;;
+      'fstype'|'disktype'|'sysdisktype')
+         LIRAM_DISK_FSTYPE="${v}"
+      ;;
+      'slot')
+         LIRAM_SLOT="${v#/}"
+      ;;
+      'size'|'rootfs_size')
+         LIRAM_ROOTFS_SIZE="${v}"
+      ;;
+      'layout')
+         LIRAM_LAYOUT="${v}"
+      ;;
+      'home_size')
+         LIRAM_HOME_TMPFS_SIZE="${v}"
+      ;;
+      'etc_size')
+         LIRAM_ETC_TMPFS_SIZE="${v}"
+      ;;
+      'usr_size')
+         LIRAM_USR_TMPFS_SIZE="${v}"
+      ;;
+      'experimental')
+         LIRAM_EXPERIMENTAL=y
+      ;;
+      *)
+         ${LOGGER} --level=WARN --facility=cmdline.liram "unknown option '${1}'"
+      ;;
+   esac
+}
+
+cmdline_parser_liram() {
+   case "${arg}" in
+      liram)
+         NEWROOT_TYPE=liram
+      ;;
+      liram=?*)
+         NEWROOT_TYPE=liram
+         F_ITER=__cmdline_parser_liram_opts list_iterator "${value}"
+      ;;
+      liram_disk=?*)
+         __cmdline_parser_liram_opts "disk=${value}"
+      ;;
+      liram_disktype=?*)
+         __cmdline_parser_liram_opts "disktype=${value}"
+      ;;
+      liram_slot=?*)
+         __cmdline_parser_liram_opts "slot=${value}"
+      ;;
+      liram_size=?*)
+         __cmdline_parser_liram_opts "size=${value}"
+      ;;
+      liram_layout=?*)
+         __cmdline_parser_liram_opts "layout=${value}"
+      ;;
+
+      # default return
+      *)
+         return 1
+      ;;
+   esac
+}
