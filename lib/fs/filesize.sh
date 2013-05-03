398fc624636d0a2b728737dfad8a47a8928bbcfb
André Erdmann <dywi@mailerd.de>
2013-05-04 01:06:03 +0200

    lib/fs/filesize: set FILESIZE >= 1

--- lib/fs/filesize.sh
+++ lib/fs/filesize.sh
@@ -8,8 +8,14 @@ get_filesize() {
    FILESIZE="-1"
    if [ -n "${1-}" ] && [ -e "${1-}" ]; then
       local size=`du -xms "${1}" | sed -r -e 's=\s.*$=='`
-      if [ -n "${size}" ] && [ ${size} -gt 0 ]; then
-         FILESIZE="${size}"
+
+      if [ -n "${size}" ]; then
+         # busybox' du returns 0 for small files
+         if [ ${size} -gt 0 ]; then
+            FILESIZE="${size}"
+         else
+            FILESIZE=1
+         fi
          return 0
       fi
    fi
