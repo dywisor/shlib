6a6f9dba2debff9ce2b1eeff30a30ba4e1292a49
André Erdmann <dywi@mailerd.de>
2013-05-03 17:13:24 +0200

    lib/fs/dosym.sh: fix test command
    It's "-h <link>", not "-l <link>".

--- lib/fs/dosym.sh
+++ lib/fs/dosym.sh
@@ -3,7 +3,7 @@
 #  Creates a link to the given target if it (the link) does not exist.
 #
 dosym() {
-   if [ -l "${2:?}" ] || [ -e "${2}" ]; then
+   if [ -h "${2:?}" ] || [ -e "${2}" ]; then
       return 0
    else
       ln -T -s -- "${1:?}" "${2}"
