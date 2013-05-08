d2eccc2a2b010898818598faabb8fd4f10671d9a
André Erdmann <dywi@mailerd.de>
2013-05-08 21:09:22 +0200

    lib/util/stdstr: pass *argv to sed/cut/...

--- lib/strutil/stdstr.sh
+++ lib/strutil/stdstr.sh
@@ -4,28 +4,36 @@
 #  and replaces any whitespace sequence within the string
 #  with a single space char.
 #
-str_trim() { sed -r -e 's,^\s+,,' -e 's,\s+$,,' -e 's,\s+, ,g'; }
+str_trim() { sed -r -e 's,^\s+,,' -e 's,\s+$,,' -e 's,\s+, ,g' "$@"; }
 
 # @function_alias void str_strip() renames sed()
 #
 #  Removes whitespace at the beginning + end of a string.
 #
-str_strip() { sed -r -e 's,^\s+,,' -e 's,\s+$,,'; }
+str_strip() { sed -r -e 's,^\s+,,' -e 's,\s+$,,' "$@"; }
 
 # @function_alias void str_rstrip() renames sed()
 #
 #  Removes whitespace at the end of a string.
 #
-str_rstrip() { sed -r -e 's,\s+$,,'; }
+str_rstrip() { sed -r -e 's,\s+$,,' "$@"; }
 
 # @function_alias void str_lstrip() renames sed()
 #
 #  Removes whitespace at the beginning of a string.
 #
-str_lstrip() { sed -r -e 's,^\s+,,'; }
+str_lstrip() { sed -r -e 's,^\s+,,' "$@"; }
 
 # @function_alias str_field ( fieldspec, field_separator=" " ) renames cut()
-str_field() { cut -d "${2- }" -f "${1}"; }
+str_field() {
+   if [ $# -lt 3 ]; then
+      cut -d "${2- }" -f "${1}"
+   else
+     local f="${1}" d="${2- }"
+     shift 2
+     cut -d "${d}" -f "${f}" "$@"
+   fi
+}
 
 # ~int revrev ( *argv )
 #
@@ -40,7 +48,7 @@ revrev() { rev | "$@" | rev; }
 revcut() { rev | cut "$@" | rev; }
 
 # @function_alias str_upper() renames tr()
-str_upper() { tr [:lower:] [:upper:]; }
+str_upper() { tr [:lower:] [:upper:] "$@"; }
 
 # @function_alias str_lower() renames tr()
-str_upper() { tr [:upper:] [:lower:]; }
+str_upper() { tr [:upper:] [:lower:] "$@"; }
