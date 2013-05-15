6350db40f14f5c97168bb8af5fc538ac6437ee4b
André Erdmann <dywi@mailerd.de>
2013-05-15 23:50:36 +0200

    make_scripts.sh: add tmp/ dir to non-flat scripts

--- make_scripts.sh
+++ make_scripts.sh
@@ -61,7 +61,7 @@ for src; do
    if yesno "${MAKESCRIPT_FLAT}"; then
       dest="${src%.sh}"
       case "${dest}" in
-         initramfs/*|lib/*)
+         initramfs/*|lib/*|tmp/*)
             dest="${MAKESCRIPT_DEST}/${dest}"
          ;;
          *)
