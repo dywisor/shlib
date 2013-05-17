1668e78e69d73653cbc264c008d59e212460a9c0
André Erdmann <dywi@mailerd.de>
2013-05-17 18:54:02 +0200

    liram: add 'setup' module
    In addition to what's provided by initramfs/newroot/setup, this module
    implements subtree mounting.

---
+++ lib/initramfs/newroot/liram/setup.sh
@@ -0,0 +1,32 @@
+# @extern int  newroot_setup_dirs        ( *file=<default> )
+# @extern int  newroot_setup_mountpoints ( fstab_file=<default> )
+# @extern int  newroot_setup_premount    ( file=<default>, **CMDLINE_FSCK )
+# @extern void newroot_setup_all()
+
+: ${NEWROOT_CONFIG_DIR?}
+
+# int liram_setup_subtrees ( *file=<detect>, **NEWROOT, **NEWROOT_CONFIG_DIR )
+#
+#  Reads subtree configuration from file(s) and mounts the subtrees
+#  accordingly.
+#
+#  Returns on success and 40 if any of the given files did not exist.
+#  Also returns 0 if no file was specified and the default did not exist.
+#
+#  This should be called as early as possible, usually after unpacking
+#  the rootfs tarball (or mounting the rootfs disk).
+#
+liram_setup_subtrees() {
+   if [ -z "${1-}" ]; then
+      set -- "${NEWROOT?}/${NEWROOT_CONFIG_DIR#/}/liram-subtree"
+      [ -f "${1}" ] || return 0
+   fi
+
+   ## file format: <mountpoint> <size_m> [<name> [<mount_opts>]]
+   ## Surprisingly, this is also what liram_mount_subtree() expects.
+   ##
+   F_ITER=liram_mount_subtree \
+   F_ITER_ON_ERROR=return \
+   ITER_UNPACK_ITEM=y ITER_SKIP_COMMENT=y ITER_SKIP_EMPTY=y \
+   file_iterator "$@"
+}
