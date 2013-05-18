594c8e6cc94d40577fa220673d4f8c389b54042c
André Erdmann <dywi@mailerd.de>
2013-05-19 00:47:09 +0200

    generate_script:sh: write output file directly
    The --output,-O arg allows to write the generated script to a file instead
    of stdout. The --chmod and --verify switches can be used to process the
    created file further.

--- generate_script.sh
+++ generate_script.sh
@@ -3,10 +3,11 @@
 set -u
 
 . "${0%/*}/loader.sh" "${0%/*}/lib" && \
-loader_load die message argparse scriptinfo || exit
+loader_load autodie die message || exit
+autodie loader_load argparse scriptinfo misc/qwhich fs/dodir_minimal
 
 HELP_DESCRIPTION="make shell scripts"
-HELP_BODY="generate shell scripts using ${SCRIPT_DIR}/shlib
+HELP_BODY="generate shell scripts using ${SCRIPT_DIR}/lib \
 and the templates from ${SCRIPT_DIR}/scripts
 
 "
@@ -17,8 +18,11 @@ HELP_OPTIONS="
                        on a big shlib file
 --shlib        (-L) -- shlib file to link against
                        (mutually exclusive with --standalone)
---bash         (-B) -- create a script that uses /bin/bash
 --interpreter  (-I) -- set interpreter (defaults to /bin/sh)
+--bash         (-B) -- set interpreter to /bin/bash and use bash modules
+--output       (-O) -- write script to file (defaults to '-', stdout)
+--chmod             -- chmod output file (defaults to 0755)
+--verify       (-C) -- perform basic script verification, needs -O
 --name         (-N) -- name of the script
 --list         (-l) -- list all available scripts
 "
@@ -30,6 +34,18 @@ list_scripts() {
 			find . -type f -name '*.sh' | cut -b 3- | rev | cut -b 4- | rev | sort
 	)
 	exit $?
+}
+
+argparse_set_output() {
+	argparse_need_arg "$@"
+	case "${1}" in
+		'-')
+			SCRIPT_OUTFILE=
+		;;
+		*)
+			SCRIPT_OUTFILE=$(readlink -m "${1}")
+		;;
+	esac
 }
 
 argparse_break() {
@@ -76,6 +92,12 @@ argparse_shortopt() {
 			argparse_need_arg "$@"
 			SCRIPT_INTERPRETER="${1}"
 		;;
+		'O')
+			argparse_set_output "$@"
+		;;
+		'C')
+			SCRIPT_VERIFY=y
+		;;
 		'N')
 			argparse_need_arg "$@"
 			IN_SCRIPT_NAME="${1}"
@@ -104,6 +126,16 @@ argparse_longopt() {
 		'interpreter')
 			argparse_need_arg "$@"
 			SCRIPT_INTERPRETER="${1}"
+		;;
+		'output')
+			argparse_set_output "$@"
+		;;
+		'verify')
+			SCRIPT_VERIFY=y
+		;;
+		'chmod')
+			argparse_need_arg "$@"
+			SCRIPT_CHMOD="${1}"
 		;;
 		'name')
 			argparse_need_arg "$@"
@@ -134,40 +166,78 @@ if [ -z "${NEEDS_SHLIB-}" ]; then
 	fi
 fi
 
-if [ "${SCRIPT_STANDALONE=n}" = "y" ]; then
-	opts="${SHLIBCC_ARGS=-u --strip-virtual}"
-	if [ "${NEEDS_SHLIB}" = "y" ]; then
-		opts="${opts} -D"
-	else
-		opts="${opts} --allow-empty --short-header"
-	fi
-	[ "${SCRIPT_BASH=n}" != "y" ] || opts="${opts} --bash"
-
-	"${SCRIPT_DIR}/CC" ${opts} --stable-sort --main "${IN_SCRIPT}"
-else
-	if [ -z "${IN_SCRIPT_NAME-}" ]; then
-		IN_SCRIPT_NAME="${IN_SCRIPT##*/}"
-		IN_SCRIPT_NAME="${IN_SCRIPT_NAME%.*}"
-	fi
-
-	: ${SHLIB_TARGET:=/sh/lib/shlib.sh}
-	{
-		echo "#!${SCRIPT_INTERPRETER:-/bin/sh}"
-		echo '# -*- coding: utf-8 -*-'
-		echo '#'
-		echo "# script ${IN_SCRIPT_NAME}"
-		echo '#'
-		if [ "${SCRIPT_BASH=n}" = "y" ]; then
-			echo "set -o nounset"
-			echo "set +o history"
-		else
-			echo "set -u"
-		fi
+makescript() {
+	if [ "${SCRIPT_STANDALONE=n}" = "y" ]; then
+		local opts="${SHLIBCC_ARGS=-u --strip-virtual}"
 		if [ "${NEEDS_SHLIB}" = "y" ]; then
-			echo
-			echo ". \"${SHLIB_TARGET}\" -- || exit"
+			opts="${opts} -D"
+		else
+			opts="${opts} --allow-empty --short-header"
 		fi
-		echo
-		grep -v ^'#![[:blank:]]*/bin' "${IN_SCRIPT}"
-	}
+		[ "${SCRIPT_BASH=n}" != "y" ] || opts="${opts} --bash"
+
+		"${SCRIPT_DIR}/CC" ${opts} --stable-sort --main "${IN_SCRIPT}"
+	else
+		if [ -z "${IN_SCRIPT_NAME-}" ]; then
+			IN_SCRIPT_NAME="${IN_SCRIPT##*/}"
+			IN_SCRIPT_NAME="${IN_SCRIPT_NAME%.*}"
+		fi
+
+		: ${SHLIB_TARGET:=/sh/lib/shlib.sh}
+		{
+			echo "#!${SCRIPT_INTERPRETER:-/bin/sh}"
+			echo '# -*- coding: utf-8 -*-'
+			echo '#'
+			echo "# script ${IN_SCRIPT_NAME}"
+			echo '#'
+			if [ "${SCRIPT_BASH=n}" = "y" ]; then
+				echo "set -o nounset"
+				echo "set +o history"
+			else
+				echo "set -u"
+			fi
+			if [ "${NEEDS_SHLIB}" = "y" ]; then
+				echo
+				echo ". \"${SHLIB_TARGET}\" -- || exit"
+			fi
+			echo
+			grep -v ^'#![[:blank:]]*/bin' "${IN_SCRIPT}"
+		}
+	fi
+}
+
+makescript_file() {
+	dodir_clean "${SCRIPT_OUTFILE%/*}" && makescript > "${SCRIPT_OUTFILE:?}"
+}
+
+if [ -z "${SCRIPT_OUTFILE-}" ]; then
+	[ "${SCRIPT_VERIFY:-n}" != "y" ] || argparse_die "--verify needs --output"
+	autodie makescript
+else
+	rc=0
+	makescript_file || rc=$?
+	if [ ${rc} -ne 0 ]; then
+		if [ "${SCRIPT_OUTFILE_REMOVE:-y}" = "y" ]; then
+			[ ! -e "${SCRIPT_OUTFILE}" ] || autodie rm -- "${SCRIPT_OUTFILE}"
+		fi
+		die "makescript_file() returned ${rc}."
+	fi
+	autodie test -s "${SCRIPT_OUTFILE}"
+
+	if [ "${SCRIPT_VERIFY:-n}" = "y" ]; then
+		if [ "${SCRIPT_BASH=n}" = "y" ]; then
+			autodie bash -n "${SCRIPT_OUTFILE}"
+		else
+			#local interpreter
+			for interpreter in "bash" "busybox ash" "dash"; do
+				if qwhich "${interpreter%% *}"; then
+					autodie ${interpreter} -n "${SCRIPT_OUTFILE}"
+				else
+					ewarn "'${interpreter}' is not available."
+				fi
+			done
+		fi
+	fi
+
+	autodie chmod ${SCRIPT_CHMOD:-0755} "${SCRIPT_OUTFILE}"
 fi
