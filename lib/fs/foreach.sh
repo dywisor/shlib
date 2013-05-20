349491e298565ecc15340ff437e49a00a2c372ac
André Erdmann <dywi@mailerd.de>
2013-05-20 12:32:00 +0200

    lib/fs: add 'foreach' module
    This module provides simple iterator functions that save you one level of
    filtering.
    
    For example, the following code snippet
    
    for cpufreq in /sys/devices/system/cpu/cpu?*/cpufreq; do
       if [ -d "${cpufreq}" ]; then
          apply_cpufreq_settings "${cpufreq}"
       fi
    done
    
    can now be written as
    
    foreach_dir_do apply_cpufreq_settings /sys/devices/system/cpu/cpu?*/cpufreq

---
+++ lib/fs/foreach.sh
@@ -0,0 +1,53 @@
+# @private int fs_foreach__do_if (
+#    test_condition, func, *fs_item, **F_FS_FOREACH_ON_ERROR=return
+# )
+# DEFINES @fs_foreach <test_condition>|<test_condition-name> <function_name>
+#  AS int <function_name> (
+#     <test_condition>, func, *fs_item, **F_FS_FOREACH_ON_ERROR
+#  )
+#
+#  Calls
+#   %func ( <fs_item> ) for each <fs_item> if test -%test_condition <fs_item>
+#
+fs_foreach__do_if() {
+   local cond="${1:?}" f="${2:?}"
+   shift 2
+   while [ $# -gt 0 ]; do
+      [ ! -${cond} "${1}" ] || ${f} "${1}" || ${F_FS_FOREACH_ON_ERROR:-return}
+      shift
+   done
+}
+
+# int fs_foreach_do_if (
+#    f_condition, func, *fs_item, **F_FS_FOREACH_ON_ERROR=return
+# )
+#
+#  Calls
+#   %func ( <fs_item> ) for each <fs_item> if %f_condition ( <fs_item> )
+#
+fs_foreach_do_if() {
+   local f_cond="${1:?}" f="${2:?}"
+   shift 2
+   while [ $# -gt 0 ]; do
+      ! ${f_cond} "${1}" || ${f} "${1}" || ${F_FS_FOREACH_ON_ERROR:-return}
+   done
+}
+
+# @fs_foreach file fs_foreach_file_do()
+# @fs_foreach file foreach_file_do()
+fs_foreach_file_do() { fs_foreach__do_if f "$@"; }
+foreach_file_do()    { fs_foreach__do_if f "$@"; }
+
+# @fs_foreach dir fs_foreach_dir_do()
+# @fs_foreach dir foreach_dir_do()
+fs_foreach_dir_do() { fs_foreach__do_if d "$@"; }
+foreach_dir_do()    { fs_foreach__do_if d "$@"; }
+
+# @fs_foreach symlink fs_foreach_symlink_do()
+# @fs_foreach symlink foreach_symlink_do()
+#
+fs_foreach_symlink_do() { fs_foreach__do_if h "$@"; }
+foreach_symlink_do()    { fs_foreach__do_if h "$@"; }
+
+# @fs_foreach e fs_foreach_do()
+fs_foreach_do() { fs_foreach__do_if e "$@"; }
