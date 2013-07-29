4ea0451637c5eb437d034331bde3835101737a77
André Erdmann <dywi@mailerd.de>
2013-07-29 20:52:00 +0200

    lib/list_redux: inline list_has() (for speed)

--- lib/list_redux.sh
+++ lib/list_redux.sh
@@ -5,8 +5,20 @@
 #
 list_redux() {
    v0=
+   local k
+   local is_new
+
    while [ $# -gt 0 ]; do
-      [ -z "${1}" ] || list_has "${1}" ${v0} || v0="${v0} ${1}"
+      if [ -n "${1}" ]; then
+         is_new=1
+         for k in ${v0}; do
+            if [ "${k}" = "${1}" ]; then
+               is_new=0
+               break
+            fi
+         done
+         [ ${is_new} -ne 1 ] || v0="${v0} ${1}"
+      fi
       shift
    done
    v0="${v0# }"
