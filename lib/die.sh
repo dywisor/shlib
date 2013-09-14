ed0cd6929c076ed2e513569cc14a6aa994022695
André Erdmann <dywi@mailerd.de>
2013-09-14 18:50:53 +0200

    lib/die, die__minimal(): accept **DIE

--- lib/die.sh
+++ lib/die.sh
@@ -1,18 +1,18 @@
-# @private @noreturn die__minimal ( message, code )
+# @private @noreturn die__minimal ( message, code, **DIE=exit )
 #
-#  Prints %message to stderr and calls exit(code) afterwards.
+#  Prints %message to stderr and calls %DIE(code) afterwards.
 #
 die__minimal() {
    [ "${HAVE_BREAKPOINT_SUPPORT:-n}" != "y" ] || breakpoint die
 
    if [ "${__HAVE_MESSAGE_FUNCTIONS:-n}" = "y" ]; then
       eerror "${1}" "died:"
-   elif [ -n "${1}" ]; then
+   elif [ -n "${1-}" ]; then
       echo "died: ${1}" 1>&2
    else
       echo "died." 1>&2
    fi
-   exit "${2}"
+   ${DIE:-exit} ${2:-2}
 }
 
 # @noreturn die ( message=, code=2, **__F_DIE=die__minimal )
