8e423231cdd893fad36c0bfe4b0367f859611b5d
André Erdmann <dywi@mailerd.de>
2013-09-29 20:52:21 +0200

    lib/scriptinfo: accept $0 as first arg

--- lib/scriptinfo.bash
+++ lib/scriptinfo.bash
@@ -1,14 +1,26 @@
 eval_scriptinfo() {
-   local x="${BASH_SOURCE[0]}"
-
-   SCRIPT_FILE="$( realpath -Ls "${x}" 2>>${DEVNULL} )"
-   if [[ -z "${SCRIPT_FILE}" ]]; then
-      SCRIPT_FILE="$(readlink -f "${x}" 2>>${DEVNULL} )"
-      [[ -n "${SCRIPT_FILE}" ]] || SCRIPT_FILE="${x}"
+   local x
+   if [[ ${#} -gt 0 ]]; then
+      x="${1}"
+   else
+      x="${BASH_SOURCE[0]}"
    fi
+
+   if [[ -n "${x}" ]]; then
+      SCRIPT_FILE="$( realpath -Ls "${x}" 2>>${DEVNULL} )"
+      if [[ -z "${SCRIPT_FILE}" ]]; then
+         SCRIPT_FILE="$(readlink -f "${x}" 2>>${DEVNULL} )"
+         [[ -n "${SCRIPT_FILE}" ]] || SCRIPT_FILE="${x}"
+      fi
+
+      SCRIPT_DIR="${SCRIPT_FILE%/*}"
+   else
+      SCRIPT_FILE="UNDEF"
+      SCRIPT_DIR="${PWD}"
+   fi
+
    SCRIPT_FILENAME="${SCRIPT_FILE##*/}"
    SCRIPT_NAME="${SCRIPT_FILENAME%.*}"
-   SCRIPT_DIR="${SCRIPT_FILE%/*}"
 }
 
 eval_scriptinfo
