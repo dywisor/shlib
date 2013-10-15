7706e74168c8f271942b965350466a36a7c83d1b
André Erdmann <dywi@mailerd.de>
2013-10-15 15:48:36 +0200

    lib/devel/configure: misc functions for configure scripts

---
+++ lib/devel/configure/base.sh
@@ -0,0 +1,94 @@
+### message functions
+
+# void configure_{echo,info,warn,error}{,n} ( message, ... )
+#
+#  Prints a normal/info/warning/error message (with/without a trailing newline
+#  char). The info, warn and error functions accepts additional args.
+#
+configure_echo()   { echo    "$@"; }
+configure_info()   { einfo   "$@"; }
+configure_warn()   { ewarn   "$@"; }
+configure_error()  { eerror  "$@"; }
+configure_echon()  { echo -n "$@"; }
+configure_infon()  { einfon  "$@"; }
+configure_warnn()  { ewarnn  "$@"; }
+configure_errorn() { eerrorn "$@"; }
+
+# void configure_check_message_begin ( message, message_header=<default> )
+#
+#  Prints a "Checking whether <message> ... " info message, optionally
+#  with the given header (instead of the default one).
+#
+#  Does not append a trailing newline char.
+#
+configure_check_message_begin() {
+   configure_infon "Checking whether ${1} ... " ${2-}
+}
+
+# void configure_check_message_end ( message )
+#
+#  Completes a "checking whether ..." message (including a trailing newline
+#  char).
+#
+configure_check_message_end() {
+   configure_echo "${1}"
+}
+
+# @function_alias configure_die() is die()
+#
+#  configure-related modules / script should call this script instead of die().
+#
+configure_die() { die "$@"; }
+
+
+# int configure_which_nonfatal ( prog_name, **v0! )
+#
+#  Returns 0 if program with the given name could be found, else 1.
+#  Stores the path to the program in %v0.
+#
+configure_which_nonfatal() {
+   v0=
+   [ $# -eq 1 ] && [ -n "${1-}" ] || \
+      configure_die "configure_which: bad usage."
+
+   configure_check_message_begin "${1} exists"
+   v0=$(which "${1}" 2>>${DEVNULL})
+   if [ -n "${v0}" ]; then
+      configure_check_message_end "${v0}"
+   else
+      configure_check_message_end "no"
+      return 1
+   fi
+}
+
+# int configure_which ( *prog_names, [**v0!] ), raises configure_die()
+#
+#  Calls configure_which_nonfatal() for each prog_name and dies on first
+#  failure.
+#
+#  Leaks %v0 iff exactly one arg is given.
+#
+configure_which() {
+   if [ $# -eq 1 ]; then
+      configure_which_nonfatal "${1}" || configure_die
+   else
+      local v0
+      while [ $# -gt 0 ]; do
+         [ -z "${1-}" ] || configure_which_nonfatal "${1}" || configure_die
+         shift
+      done
+   fi
+}
+
+# void configure_which_which(), raises configure_die()
+#
+#  Verifies that "which" can be found.
+#
+configure_which_which() {
+   if configure_which_nonfatal which; then
+      return 0
+   else
+      configure_die \
+         "${SCRIPT_NAME} cannot detect whether programs are available."
+   fi
+}
---
+++ lib/devel/configure/python.sh
@@ -0,0 +1,125 @@
+# void configure_which_python ( version="", **PYTHON! )
+#
+#  Locates python and stores its path in %PYTHON.
+#
+configure_which_python() {
+   local v0
+   configure_which python${1-} && PYTHON="${v0}"
+}
+
+# void configure_which_python2 ( subversion="", **PYTHON!, **PYTHON2! )
+#
+#  Locates python2 and stores its path in both %PYTHON and %PYTHON2.
+#
+configure_which_python2() {
+   local v0
+   local subver=
+   [ -z "${1-}" ] || subver=".${1#.}"
+   configure_which python2${subver} && PYTHON="${v0}" && PYTHON2="${v0}"
+}
+
+# void configure_which_python3 ( subversion="", **PYTHON!, **PYTHON3! )
+#
+#  Locates python3 and stores its path in both %PYTHON and %PYTHON3.
+#
+configure_which_python3() {
+   local v0
+   local subver=
+   [ -z "${1-}" ] || subver=".${1#.}"
+   configure_which python3${subver} && PYTHON="${v0}" && PYTHON3="${v0}"
+}
+
+
+# int configure_python_has_module ( *modules, **PYTHON, **v0! )
+#
+#  Checks whether the given python modules can be imported.
+#  Returns the number of missing modules or 255, whatever is lower.
+#  Also stores the names of the missing modules in %v0.
+#
+#  %PYTHON has to be set before calling this function, which is automatically
+#  done by configure_which_python*().
+#
+configure_python_has_module() {
+   v0=
+   local name
+   local fail=0
+   [ -n "${PYTHON-}" ] || configure_die "\$PYTHON is not set."
+   for name; do
+      configure_check_message_begin "${PYTHON} has the '${name}' module"
+      if ${PYTHON} -c "import ${name}" 2>>${DEVNULL}; then
+         configure_check_message_end "yes"
+      else
+         configure_check_message_end "no"
+         v0="${v0} ${name}"
+         fail=$(( ${fail} + 1 ))
+      fi
+   done
+   v0="${v0# }"
+   [ ${fail} -lt 256 ] || fail=255
+   return ${fail}
+}
+
+# void configure_python_need_module ( *modules, **PYTHON )
+#
+#  Like configure_python_has_module(), but dies if any module could not
+#  be imported (after trying to import all modules).
+#
+configure_python_need_module() {
+   local v0
+   configure_python_has_module "$@" || \
+      configure_die "essential python modules are missing: ${v0}"
+}
+
+# @function_alias configure_python_check_import()
+#  renames configure_python_need_module()
+configure_python_check_import() { configure_python_need_module "$@"; }
+
+
+# int configure_python_try_import_from ( module, *names, **PYTHON, **v0! )
+#
+#  Tries to import one or more names from the given python module.
+#  Returns the number of names that couldn't be imported or 255, whatever
+#  is lower. Also stores the "bad" names in %v0.
+#
+#  %PYTHON has to be set before calling this function, which is automatically
+#  done by configure_which_python*().
+#
+configure_python_try_import_from() {
+   v0=
+   [ -n "${1-}" ] && [ -n "${2-}" ] || \
+      configure_die "configure_python_try_import_from(): bad usage."
+   [ -n "${PYTHON-}" ] || configure_die "\$PYTHON is not set."
+
+   local mod="${1}"
+   local fail=0
+   shift
+   for name; do
+      configure_check_message_begin \
+         "'${name}' can be imported from module '${mod}'"
+      if ${PYTHON} -c "from ${mod} import ${name}" 2>>${DEVNULL}; then
+         configure_check_message_end "yes"
+      else
+         configure_check_message_end "no"
+         v0="${v0} ${name}"
+         fail=$(( ${fail} + 1 ))
+      fi
+   done
+   v0="${v0# }"
+   [ ${fail} -lt 256 ] || fail=255
+   return ${fail}
+}
+
+# void configure_python_check_import_from ( module, *names, **PYTHON )
+#
+#  Like configure_python_try_import_from(), but dies if any name could not
+#  be imported (after trying to import all names).
+#
+configure_python_check_import_from() {
+   local v0
+   [ -n "${1-}" ] || \
+      configure_die "configure_python_check_import_from(): bad usage."
+
+   configure_python_need_module "${1}"
+   configure_python_try_import_from "$@" || \
+      configure_die "cannot import names from module '${1}': ${v0}"
+}
