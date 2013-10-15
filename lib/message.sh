b73e94f8c84163300e1371ff4d3cc923099a0d77
André Erdmann <dywi@mailerd.de>
2013-10-15 15:47:51 +0200

    lib/message.sh: add no-newline message functions

--- lib/message.sh
+++ lib/message.sh
@@ -1,20 +1,33 @@
-# void {einfo,ewarn,eerror}_nocolor ( message, header=<INFO,WARN,ERROR> )
+# void {einfo,ewarn,eerror}{,n}_nocolor ( message, header=<INFO,WARN,ERROR> )
 #
 #  Prints ${header}${message} to stdout.
 #
-einfo_nocolor()   { printf "${2:-[INFO]}${1:+ }${1-}\n"; }
-ewarn_nocolor()   { printf "${2:-[WARN]}${1:+ }${1-}\n"; }
-eerror_nocolor()  { printf "${2:-[ERROR]}${1:+ }${1-}\n"; }
+einfo_nocolor()   { printf -- "${2:-[INFO]}${1:+ }${1-}\n"; }
+ewarn_nocolor()   { printf -- "${2:-[WARN]}${1:+ }${1-}\n"; }
+eerror_nocolor()  { printf -- "${2:-[ERROR]}${1:+ }${1-}\n"; }
+
+einfon_nocolor()   { printf -- "${2:-[INFO]}${1:+ }${1-}"; }
+ewarnn_nocolor()   { printf -- "${2:-[WARN]}${1:+ }${1-}"; }
+eerrorn_nocolor()  { printf -- "${2:-[ERROR]}${1:+ }${1-}"; }
 
 # void __message_colored ( text_colored, color, text_nocolor )
 #
 #  Prints <text_colored><text_nocolor> using the specified color.
 #
 __message_colored() {
-   printf "\033[${2:?}${1:?}\033[0m${3:+ }${3-}\n"
+   printf -- "\033[${2:?}${1:?}\033[0m${3:+ }${3-}\n"
 }
 
-# void {einfo,ewarn,eerror}_color ( message, header=<*|INFO,WARN,ERROR> )
+# void __messagen_colored ( text_colored, color, text_nocolor ):
+#
+#  Like __message_colored(), but doesn't append a trailing newline.
+#
+__messagen_colored() {
+   printf -- "\033[${2:?}${1:?}\033[0m${3:+ }${3-}"
+}
+
+
+# void {einfo,ewarn,eerror}{,n}_color ( message, header=<*|INFO,WARN,ERROR> )
 #
 # Prints ${header}${message} to stdout (colored output).
 #
@@ -26,6 +39,10 @@ __message_colored() {
 einfo_color()  { __message_colored "${2:-*}" '1;32m' "${1-}"; }
 ewarn_color()  { __message_colored "${2:-*}" '1;33m' "${1-}"; }
 eerror_color() { __message_colored "${2:-*}" '1;31m' "${1-}"; }
+
+einfon_color()  { __messagen_colored "${2:-*}" '1;32m' "${1-}"; }
+ewarnn_color()  { __messagen_colored "${2:-*}" '1;33m' "${1-}"; }
+eerrorn_color() { __messagen_colored "${2:-*}" '1;31m' "${1-}"; }
 
 # void message_bind_functions ( **NO_COLOR=n )
 #
@@ -46,12 +63,22 @@ message_bind_functions() {
       eerror()  { eerror_color "$@" 1>&2; }
       message() { __message_colored "$*" '1;29m'; }
 
+      einfon()   { einfon_color  "$@"; }
+      ewarnn()   { ewarnn_color  "$@" 1>&2; }
+      eerrorn()  { eerrorn_color "$@" 1>&2; }
+      messagen() { __messagen_colored "$*" '1;29m'; }
+
    else
 
-      einfo()   { einfo_nocolor  "$@"; }
-      ewarn()   { ewarn_nocolor  "$@" 1>&2; }
-      eerror()  { eerror_nocolor "$@" 1>&2; }
-      message() { printf "${*}\n"; }
+      einfo()    { einfo_nocolor  "$@"; }
+      ewarn()    { ewarn_nocolor  "$@" 1>&2; }
+      eerror()   { eerror_nocolor "$@" 1>&2; }
+      message()  { printf -- "${*}\n"; }
+
+      einfon()   { einfon_nocolor  "$@"; }
+      ewarnn()   { ewarnn_nocolor  "$@" 1>&2; }
+      eerrorn()  { eerrorn_nocolor "$@" 1>&2; }
+      messagen() { printf -- "${*}"; }
 
    fi
    __HAVE_MESSAGE_FUNCTIONS=y
@@ -66,6 +93,17 @@ message_bind_functions() {
 veinfo() {
    if __verbose__ || __debug__; then
       einfo "$@"
+   fi
+   return 0
+}
+
+# void veinfon ( message, header, **DEBUG= )
+#
+#  Like veinfo(), but doesn't append a trailing newline.
+#
+veinfon() {
+   if __verbose__ || __debug__; then
+      einfon "$@"
    fi
    return 0
 }
