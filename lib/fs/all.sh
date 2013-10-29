c0505b336da40198142418f4afbd1e7449126c10
André Erdmann <dywi@mailerd.de>
2013-10-30 00:02:18 +0100

    big lib: add includes

--- lib/fs/all.sh
+++ lib/fs/all.sh
@@ -8,4 +8,5 @@ EXPORT_FUNCTIONS \
    symlink_replace \
    get_fslevel get_fslevel_diff \
    atomic_file_counter \
-   atomic_file atomic_file_do
+   atomic_file atomic_file_do \
+   print_abspath print_realpath print_realpath_safe
--- lib/strutil/all.sh
+++ lib/strutil/all.sh
@@ -1,4 +1,5 @@
 EXPORT_FUNCTIONS \
    charwise \
    str_trim str_strip str_rstrip str_lstrip str_field str_upper str_lower \
-   yesno yesno_strict
+   yesno yesno_strict \
+   fnmatch fnmatch_all fnmatch_any fnmatch_none
