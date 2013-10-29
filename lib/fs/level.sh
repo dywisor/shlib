f72778c871bbbe6dd02ef9ce123043f9d5436f2c
André Erdmann <dywi@mailerd.de>
2013-10-29 23:39:24 +0100

    clean up/improve lib/fs/level

--- lib/fs/level.sh
+++ lib/fs/level.sh
@@ -1,29 +1,21 @@
-# int get_fslevel ( fspath )
+# @private int fslevel__get_level ( normalized_fspath, **v0! )
 #
-#  Determines the level of the given filesystem path and stores the result
-#  in v0 if successful.
+#  See get_fs_level().
 #
-#  Expects canonical filesystem paths, trailing slash chars are accepted.
-#
-get_fslevel() {
+fslevel__get_level() {
+   : ${1?}
    v0=
-   local a="${1?}" b="${1%/}"
 
-   # remove trailing "/" chars
-   while [ "x${a}" != "x${b}" ]; do
-      a="${b%/}"
-      b="${a%/}"
-   done
-
-   if [ -z "${a}" ]; then
+   if [ -z "${1}" ] || [ "${1}" = "/" ]; then
       v0=0
    else
+      local b="${1}"
+      local a="${b%/*}"
       local i=0
 
-      b="${a}"
-      a="${b%/*}"
+      #[ "${a#/}" = "${a}" ] || i=1
 
-      while [ "x${a}" != "x${b}" ]; do
+      while [ "${a}" != "${b}" ]; do
          i=$(( ${i} + 1 ))
          b="${a}"
          a="${b%/*}"
@@ -35,29 +27,71 @@ get_fslevel() {
    [ -n "${v0}" ]
 }
 
+# int get_fslevel ( fspath, **v0! )
+#
+#  Determines the level of the given filesystem path and stores the result
+#  in v0 if successful.
+#
+#  The file system level is defined as the number of slashes in %fspath
+#  followed by a non-empty char sequence (/a/b->2,/a//b->3).
+#  A more strict definition would require the char sequence not to
+#  contain the slash char (/a/b->2,/a//b->2; not implemented).
+#
+#  The path is cleaned up before determining the level.
+#
+get_fslevel() {
+   fspath_remove_trailing_slashes "${1?}"
+   fslevel__get_level "${v0}"
+}
+
 # @function_alias get_fs_level() renames get_fslevel()
 #
 get_fs_level() { get_fslevel "$@"; }
 
-# int get_fslevel_diff ( fspath1, fspath2 )
+# int get_fslevel_diff ( fspath1, fspath2, **v0! )
 #
-#  Compares the level of two filesystem paths and returns the difference
-#  level(fspath1) - level(fspath2) via %v0.
+#  Compares the level of two filesystem paths where %fspath1 is the root of
+#  %fspath2 (or vice versa) and returns the difference
+#  level(fspath2) - level(fspath1) via %v0.
 #
 get_fslevel_diff() {
-   if get_fslevel "${1?}"; then
-      local x="${v0?}"
-      if get_fslevel "${2?}"; then
-         v0=$(( ${x} - ${v0} ))
+   : ${1?} ${2?}
+   local a b x negate=0
+   local root sub
+
+   fspath_remove_trailing_slashes "${1}"
+   #get_abspath "${1}"
+   root="${v0}"
+
+   fspath_remove_trailing_slashes "${2}"
+   #get_abspath "${1}"
+   sub="${v0}"
+
+   case "${sub}" in
+      "${root%/}/"*)
+         x="${sub#${root%/}}"
+      ;;
+      "${root}")
+         v0=0
          return 0
-      else
-         v0=
-         return 3
-      fi
-   else
-      v0=
-      return 2
-   fi
+      ;;
+      *)
+         case "${root}" in
+            "${sub%/}/"*)
+               negate=1
+               x="${root#${sub%/}}"
+            ;;
+            *)
+               v0=
+               return 1
+            ;;
+         esac
+      ;;
+   esac
+
+   get_fslevel "${x}"
+   [ ${negate} -eq 0 ] || v0="-${v0}"
+   return 0
 }
 
 # @function_alias get_fs_level_diff() renames get_fslevel_diff()
