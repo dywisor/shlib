201a37c5cd38f7a109c927017f170fdb4c0b06bf
André Erdmann <dywi@mailerd.de>
2013-11-01 00:11:04 +0100

    lib/function/annotate: in-code documentation

--- lib/function/annotate.sh
+++ lib/function/annotate.sh
@@ -1,3 +1,26 @@
+# @funcdef <return type> @override <function name> ( *args )
+#
+#  A function that overrides (redefines) an already defined function.
+#
+
+# @funcdef <return type> @dont-override <function name> ( *args )
+#
+#  A function that must not redefined an existing one.
+#
+
+# @funcdef <return type> @can-override <function name> ( *args )
+#
+#  A function that is expected to be overridden in order to be useful.
+#  @can-override functions usually do nothing.
+#
+
+# void OVERRIDE_FUNCTION ( function_name ), raises die()
+#
+#  Helper function for @override.
+#  Dies if %function_name is not defined and unsets the function otherwise.
+#
+#  Has to be called _before_ redefining the function.
+#
 OVERRIDE_FUNCTION() {
    if function_defined "${1:?}"; then
       unset -f "${1}"
@@ -6,8 +29,19 @@ OVERRIDE_FUNCTION() {
       die "@override: function ${1} is not defined and thus cannot be overridden."
    fi
 }
+
+# @function_alias FOVERWRITE() renames OVERRIDE_FUNCTION()
+#
 FOVERWRITE() { OVERRIDE_FUNCTION "$@"; }
 
+
+# void DONT_OVERRIDE_FUNCTION ( function_name ), raises die()
+#
+#  Helper function for @dont-override.
+#  Dies if %function_name is defined, else does nothing.
+#
+#  Has to be called _before_ defining the function.
+#
 DONT_OVERRIDE_FUNCTION() {
    if function_defined "${1:?}"; then
       die "@dont-override: function ${1} is already defined."
@@ -15,4 +49,7 @@ DONT_OVERRIDE_FUNCTION() {
       return 0
    fi
 }
+
+# @function_alias NOT_OVERRIDING() renames DONT_OVERRIDE_FUNCTION()
+#
 NOT_OVERRIDING() { DONT_OVERRIDE_FUNCTION "$@"; }
