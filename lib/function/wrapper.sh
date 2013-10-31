f7a54a75a94b7dafed478458c6c3640cbda2d3c6
André Erdmann <dywi@mailerd.de>
2013-11-01 00:10:51 +0100

    lib/function/wrapper: eval_nullfunc()

--- lib/function/wrapper.sh
+++ lib/function/wrapper.sh
@@ -11,6 +11,17 @@ ${func_name}() {
 }"
 }
 
+# void eval_nullfunc ( *func_name )
+#
+#  Creates zero or more no-op functions.
+#
+eval_nullfunc() {
+   while [ $# -gt 0 ]; do
+      [ -z "${1}" ] || eval_function "${1}" "return 0"
+      shift
+   done
+}
+
 # void function_alias ( func, func_alias, *pos_args )
 #
 #  Creates a function alias %func_alias->%func(*pos_args,*args).
