3dd90eef34c9e15130c805ba79ae9910af57ca68
André Erdmann <dywi@mailerd.de>
2013-12-22 17:50:54 +0100

    lib/fs/uncompress: compress_get_*_opt()
    + compress_get_tar_opt()
    + compress_get_mksfs_opt()
    + compress_get_name()

--- lib/fs/uncompress.sh
+++ lib/fs/uncompress.sh
@@ -2,55 +2,177 @@
 
 # @private int compress__detect_format (
 #    compression_format|file,
-#    **decompress_args!, **compress_exe!
+#    **decompress_args!, **compress_exe!, **compress_name!
 # )
 #
-#  Sets the compress_exe/decompress_args variables.
+#  Sets the compress_exe/decompress_args/compress_name variables.
 #
 #  Returns 0 on success, 2 if the first arg is empty, else 1.
 #
 compress__detect_format() {
    decompress_args='-d -c'
    compress_exe=
+   compress_name=
    case "${1##*.}" in
       '')
+         decompress_args=
          return 2
       ;;
       gzip|gz|tgz)
          compress_exe=gzip
+         compress_name="${compress_exe}"
       ;;
       bzip2|bz2|tbz2)
          compress_exe=bzip2
+         compress_name="${compress_exe}"
       ;;
       xz|txz)
          compress_exe=xz
+         compress_name="${compress_exe}"
       ;;
       lzo|lzop)
          compress_exe=lzop
+         compress_name=lzo
       ;;
       *)
          decompress_args=
+         compress_name="${compress_exe}"
          return 1
       ;;
    esac
    return 0
 }
 
+# @private int compress__set_tar_opt (
+#    **compress_name, **compress_exe, **compress_tar_opt!
+# )
+#
+#  Sets the tar options based on %compress_name/%compress_exe.
+#
+#  Returns 0 if %compress_name is supported, else 2.
+#
+compress__set_tar_opt() {
+   case "${compress_name}" in
+      gzip)
+         compress_tar_opt="-z"
+      ;;
+      bzip2)
+         compress_tar_opt="-j"
+      ;;
+      xz)
+         compress_tar_opt="-J"
+      ;;
+      lzo)
+         compress_tar_opt="--lzop"
+      ;;
+      lzma|lzip)
+         compress_tar_opt="--${compress_name}"
+      ;;
+      *)
+         if [ -n "${compress_exe}" ]; then
+            compress_tar_opt="-I ${compress_exe}"
+         else
+            return 2
+         fi
+      ;;
+   esac
+   return 0
+}
 
-#@section functions
+# @private int compress__set_mksfs_opt (
+#    **compress_name, **compress_mksfs_opt!
+# )
+#
+#  Sets the mksquashfs options based on %compress_name/%compress_exe.
+#
+#  Returns 0 if %compress_name is supported, else 2.
+#
+compress__set_mksfs_opt() {
+   case "${compress_name}" in
+      gzip|xz|lzo)
+         compress_mksfs_opt="-comp ${compress_name}"
+      ;;
+      *)
+         return 2
+      ;;
+   esac
+   return 0
+}
 
-# void compress_detect_taropt ( compression_format|file )
+# int compress_get_name ( compression_format|file, **compress_name! )
+#
+#  Sets the compress_name variable.
+#
+#  Returns 0 on success, 2 if the first arg is empty, else 1.
+#
+compress_get_name() {
+   local compress_exe decompress_args
+   compress__detect_format "$@"
+}
+
+# int compress_get_tar_opt (
+#    compression_format|file, **compress_name!, **compress_tar_opt!
+# )
+#
+#  Guesses the compression name and its tar option(s).
+#
+#  Returns 0 on success, 1 if the compression format could not be guessed,
+#  and 2 if it is unsupported.
+#
+compress_get_tar_opt() {
+   compress_name=
+   compress_tar_opt=
+   local compress_exe decompress_args
+
+   if [ -z "${1?}" ]; then
+      return 0
+   elif ! compress__detect_format "$@"; then
+      return 1
+   elif ! compress__set_tar_opt; then
+      return 2
+   else
+      return 0
+   fi
+}
+
+# @function_alias compress_get_taropt() renames compress_get_tar_opt()
+#
+compress_get_taropt() { compress_get_tar_opt "$@"; }
+
+# void compress_detect_taropt ( compression_format|file, **v0! )
 #
 #  Guesses tar compression options and stores the result in %v0.
 #
 compress_detect_taropt() {
-   v0=
-   local decompress_args compress_exe
-
-   if compress__detect_format "${1?}"; then
-      v0="--${compress_exe}"
-   fi
+   local compress_name compress_tar_opt
+   compress_get_tar_opt "$@"
+   v0="${compress_tar_opt?}"
    return 0
+}
+
+# int compress_get_mksfs_opt (
+#    compression_format|file, **compress_name!, **compress_mksfs_opt!
+# )
+#
+#  Guesses the compression name and its mksquashfs option(s).
+#
+#  Returns 0 on success, 1 if the compression format could not be guessed,
+#  and 2 if it is unsupported.
+#
+compress_get_mksfs_opt() {
+   compress_name=
+   compress_mksfs_opt=
+   local compress_exe decompress_args
+
+   if [ -z "${1?}" ]; then
+      return 0
+   elif ! compress__detect_format "$@"; then
+      return 1
+   elif ! compress__set_mksfs_opt; then
+      return 2
+   else
+      return 0
+   fi
 }
 
 # int do_uncompress ( compression_format|file, *argv )
@@ -69,7 +191,8 @@ compress_detect_taropt() {
 #    while do_uncompress ( "gz", "gz" ) will uncompress a file named "gz".
 #
 do_uncompress() {
-   local compress_exe decompress_args
+   local compress_exe decompress_args compress_name
+
    if compress__detect_format "${1-}"; then
       if [ "${1##*.}" != "${1}" ] || shift; then
          ${compress_exe} ${decompress_args} "$@"
@@ -89,6 +212,6 @@ do_uncompress() {
 #  Also verifies that the binary used for (de-)compression actually exists.
 #
 compress_supports() {
-   local compress_exe decompress_args
+   local compress_exe decompress_args compress_name
    compress__detect_format "${1-}" && qwhich "${compress_exe}"
 }
