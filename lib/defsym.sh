529f364a54580ae0a11c7103c12b351eedebc282
André Erdmann <dywi@mailerd.de>
2013-12-22 17:46:36 +0100

    lib/defsym: __not__(), __run__()

--- lib/defsym.sh
+++ lib/defsym.sh
@@ -12,3 +12,17 @@ readonly EX_OK=0
 readonly EX_ERR=1
 readonly EX_USAGE=64
 readonly ERR_FUNC_UNDEF=101
+
+#@section functions
+
+# int __run__ ( *cmdv )
+#
+#  Simply runs *cmdv.
+#
+__run__() { "$@"; }
+
+# int __not__ ( *cmdv )
+#
+#  Runs *cmdv and returns the negated returncode (1 on success, else 0).
+#
+__not__() { ! "$@"; }
