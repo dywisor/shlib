5313dafdaa26bad47720f51ed2610d201e108973
André Erdmann <dywi@mailerd.de>
2013-12-23 01:08:53 +0100

    lib/function/functrace: get_functrace()

--- lib/function/functrace.bash
+++ lib/function/functrace.bash
@@ -1,18 +1,40 @@
 #@section functions
 
+# int get_functrace ( **ftrace! )
+#
+get_functrace() {
+   ftrace=
+   set -- ${FUNCNAME[*]}
+   while [[ ${#} -gt 1 ]] && shift; do
+      case "${1}" in
+         source|die__minimal|die__extended|die__function|die__autodie)
+            ftrace+=" ((${1}))"
+         ;;
+         main)
+            if [[ ${#} -eq 1 ]]; then
+               ftrace+=" ((${1}))"
+            else
+               ftrace+=" ${1}"
+            fi
+         ;;
+         *)
+            ftrace+=" ${1}"
+         ;;
+      esac
+   done
+   ftrace="${ftrace# }"
+   return 0
+}
+
 # void print_functrace ( message_function=**F_FUNCTRACE_MSG=ewarn )
 #
 #  Prints the function backtrace (this function excluded)
 #  using a message function (see the message module).
 #
 print_functrace() {
-   local ftrace="${FUNCNAME[*]}" kw
-   ftrace=" ${ftrace#${FUNCNAME} } "
-   for kw in 'source' 'die__minimal' 'die__extended' 'die__function'; do
-      ftrace="${ftrace// ${kw} / ((${kw})) }"
-   done
-   ftrace="${ftrace# }"
-   ${1:-${F_FUNCTRACE_MSG:-ewarn}} "${ftrace% }" "[FUNCTRACE]"
+   local ftrace
+   get_functrace
+   ${1:-${F_FUNCTRACE_MSG:-ewarn}} "${ftrace#print_functrace }" "[FUNCTRACE]"
 }
 
 #@section module_features
--- lib/function/functrace.sh
+++ lib/function/functrace.sh
@@ -1,5 +1,12 @@
 #@section functions
 
+# int get_functrace ( **ftrace! )
+#
+get_functrace() {
+   ftrace=
+   return 1
+}
+
 # void print_functrace ( message_function=**F_FUNCTRACE_MSG=ewarn )
 #
 #  Function stub since %FUNCNAME is not available in sh.
