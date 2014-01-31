1cf68263b8a6bb26529eb90fc9f8158d716e9ff6
André Erdmann <dywi@mailerd.de>
2014-02-01 00:46:35 +0100

    scripts/initramfs/telinit: don't depend on busybox
    Search in /bin, /sbin for swapoff/umount/...

--- scripts/initramfs/telinit.sh
+++ scripts/initramfs/telinit.sh
@@ -1,25 +1,43 @@
 #!/bin/busybox ash
 
+# int xprog ( prog_name, *argv )
+#
+xprog() {
+   [ -n "${1-}" ] || return 100
+   local x name="${1}"; shift
+
+   for x in /bin/${name} /sbin/${name}; do
+      if [ -x "${x}" ]; then
+         ${x} "$@"
+         return ${?}
+      fi
+   done
+
+   # fall back to busybox
+   /bin/busybox ${name} "$@"
+}
+
+
 # stop logging
 for p in klogd syslogd; do
-   /bin/busybox killall "${p}"
+   xprog killall "${p}"
 done
 
 # unmount what's left
-/bin/busybox sync
+xprog sync
 
 if [ ! -e /proc/self ] || [ -e /proc/swaps ]; then
-   /bin/busybox swapoff -a
+   xprog swapoff -a
 fi
 
-/bin/busybox umount -n -a -r
+xprog umount -n -a -r
 
 # call real telinit (if any) or poweroff/reboot
 
 if [ "${0}" != "/sbin/telinit" ] && [ -x "/sbin/telinit" ]; then
    /sbin/telinit "$@"
 elif [ "${1-}" = "0" ]; then
-   /bin/busybox poweroff -f
+   xprog poweroff -f
 elif [ "${1-}" = "6" ]; then
-   /bin/busybox reboot -f
+   xprog reboot -f
 fi
