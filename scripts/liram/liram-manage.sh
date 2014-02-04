a27b9fdb8102398281a86ce5c633eed374f26156
André Erdmann <dywi@mailerd.de>
2014-02-04 23:07:19 +0100

    liram-manage: (un)mount with -M/-U

--- scripts/liram/liram-manage.sh
+++ scripts/liram/liram-manage.sh
@@ -5,9 +5,10 @@ cat << END_OF_HELP
 ${SCRIPT_NAME} -- basic liram maintenance
 
 Provides the following tools:
-[* pack: pack the current system into a new slot]
+* pack: pack the current system into a new slot
+* update-core: fetch/update core images
+* mount/umount a liram disk (sysdisk, boot)
 [* kernup: fetch/update kernel]
-[* update-core: fetch/update core images]
 [* fixup: try to fix issues]
 
 
@@ -18,8 +19,10 @@ Options:
   -V, --version          print the version and exit
   -s, --slot <name>      set the base name of the new slot (--pack)
   -P, --pack             set command to 'pack'
+  -u, --update-core      set command to 'update-core'
+  -M, --mount            mount <arg>... (default: sysdisk)
+  -U, --umount           unmount <arg>... (default: sysdisk)
   -K, --kernup           set command to 'kernup'      [TODO]
-  -U, --update-core      set command to 'update-core' [TODO]
   -F, --fixup            set command to 'fixup'       [TODO]
 END_OF_HELP
 }
@@ -58,8 +61,16 @@ while [ ${#} -gt 0 ]; do
          # **LIRAM_MANAGE_X_UPDATE_KERNEL
          SCRIPT_MODE="kernup"
       ;;
-      '--update-core'|'-U')
+      '--update-core'|'-u')
          SCRIPT_MODE="update-core"
+      ;;
+      '-M'|'--mount')
+         SCRIPT_MODE="mount"
+      ;;
+      '-U'|'--umount'|'--unmount')
+         : ${LIRAM_DISK_MOUNT_RESTORE:=do_umount}
+         : ${LIRAM_BOOTDISK_MOUNT_RESTORE:=do_umount}
+         SCRIPT_MODE="umount"
       ;;
       '--help'|'--usage'|'-h')
          main_print_help
@@ -94,6 +105,11 @@ case "${SCRIPT_MODE}" in
    ;;
    'kernup'|'fixup')
       die "script mode '${SCRIPT_MODE}' is not implemented."
+   ;;
+   'mount'|'umount')
+      for name in ${POS_ARGS:-sysdisk}; do
+         liram_manage_${SCRIPT_MODE}_${name}
+      done
    ;;
    *)
       die "unknown script mode ${SCRIPT_MODE}."
