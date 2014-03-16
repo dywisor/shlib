0a3ed36f2a2b7d2bd1b062a45047602ec4043a80
André Erdmann <dywi@mailerd.de>
2014-03-16 03:30:04 +0100

    lib/strutil/yesno: word_is_yes(), word_is_no()

--- lib/strutil/yesno.sh
+++ lib/strutil/yesno.sh
@@ -1,17 +1,11 @@
 #@section functions
 
-# int yesno ( word )
+# int word_is_yes ( word )
 #
-#  Returns:
-#  * YESNO_YES   (0) if word means yes
-#  * YESNO_EMPTY (2) if word is empty
-#  * YESNO_NO    (1) otherwise (word is not empty and does not mean yes)
+#  Returns 0 if %word means "yes", else 1.
 #
-yesno() {
+word_is_yes() {
    case "${1-}" in
-      '')
-         return ${YESNO_EMPTY:-2}
-      ;;
       # yes | y | true | 1 | enable(d) | on
       [yY][eE][sS]|\
       [yY]|\
@@ -20,12 +14,47 @@ yesno() {
       [eE][nN][aA][bB][lL][eE]?|\
       [oO][nN]\
       )
-         return ${YESNO_YES:-0}
-      ;;
-      *)
-         return ${YESNO_NO:-1}
+         return 0
       ;;
    esac
+   return 1
+}
+
+# int word_is_no ( word )
+#
+#  Returns 0 if %word means "no", else 1.
+#
+word_is_no() {
+   case "${1-}" in
+      # no | n | false | 0 | disable(d) | off
+      [nN][oO]|\
+      [nN]|\
+      [fF][aA][lL][sS][eE]|\
+      0|\
+      [dD][iI][sS][aA][bB][lL][eE]?|\
+      [oO][fF][fF]\
+      )
+         return 0
+      ;;
+   esac
+   return 1
+}
+
+# int yesno ( word )
+#
+#  Returns:
+#  * YESNO_YES   (0) if word means yes
+#  * YESNO_EMPTY (2) if word is empty
+#  * YESNO_NO    (1) otherwise (word is not empty and does not mean yes)
+#
+yesno() {
+   if [ -z "${1-}" ]; then
+      return ${YESNO_EMPTY:-2}
+   elif word_is_yes "${1}"; then
+      return ${YESNO_YES:-0}
+   else
+      return ${YESNO_NO:-1}
+   fi
 }
 
 # int yesno_strict ( word )
@@ -37,24 +66,13 @@ yesno() {
 #  * YESNO_UNDEF (3) if word means neither yes nor no
 #
 yesno_strict() {
-   local rc=0
-   yesno "$@" || rc=$?
-
-   [ ${rc} -eq ${YESNO_NO:-1} ] || return ${rc}
-
-   case "${1-}" in
-      # no | n | false | 0 | disable(d) | off
-      [nN][oO]|\
-      [nN]|\
-      [fF][aA][lL][sS][eE]|\
-      0|\
-      [dD][iI][sS][aA][bB][lL][eE]?|\
-      [oO][fF][fF]\
-      )
-         return ${YESNO_NO:-1}
-      ;;
-      *)
-         return ${YESNO_UNDEF:-3}
-      ;;
-   esac
+   if [ -z "${1-}" ]; then
+      return ${YESNO_EMPTY:-2}
+   elif word_is_yes "${1}"; then
+      return ${YESNO_YES:-0}
+   elif word_is_no "${1}"; then
+      return ${YESNO_NO:-1}
+   else
+      return ${YESNO_UNDEF:-3}
+   fi
 }
