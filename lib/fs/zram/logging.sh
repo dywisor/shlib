de31b494c430280abb626c08825a016a12ef342c
André Erdmann <dywi@mailerd.de>
2014-05-19 17:54:39 +0200

    zram: add logger shortcut functions

--- lib/fs/zram/logging.sh
+++ lib/fs/zram/logging.sh
@@ -1,18 +1,30 @@
 #@section functions
 
-# @logger zram_log ( <var args>, **ZRAM_DEV= )
+# @logger zram_log ( <var args>, **ZRAM_NAME= )
 #
 zram_log() {
    ${LOGGER} +zram.${ZRAM_NAME:-main} "${@}"
 }
 
-# @logger INFO zram_log_info ( <var args>, **ZRAM_DEV= )
+# @logger DEBUG zram_log_debug ( <var args>, **ZRAM_NAME= )
+#
+zram_log_debug() {
+   zram_log "${@}" --level=DEBUG
+}
+
+# @logger INFO zram_log_info ( <var args>, **ZRAM_NAME= )
 #
 zram_log_info() {
    zram_log "${@}" --level=INFO
 }
 
-# @logger ERROR zram_log_error ( <var args>, **ZRAM_DEV= )
+# @logger WARN zram_log_warn ( <var args>, **ZRAM_NAME= )
+#
+zram_log_warn() {
+   zram_log "${@}" --level=WARN
+}
+
+# @logger ERROR zram_log_error ( <var args>, **ZRAM_NAME= )
 #
 zram_log_error() {
    zram_log "${@}" --level=ERROR
